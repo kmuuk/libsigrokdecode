26ecb1efc75d0e869aaa112c556122f6cc692acb
Priit Laes <priit.laes@paf.com>
2021-09-03 13:25:44 +0300

    decoder: pn532: Fix copyright assignment

--- decoders/pn532/pd.py
+++ decoders/pn532/pd.py
@@ -1,7 +1,7 @@
 ##
 ## This file is part of the libsigrokdecode project.
 ##
-## Copyright (C) 2018 Stephan Thiele <stephan.thiele@mailbox.org>
+## Copyright (C) 2020 Luca Cristaldi <wifasoi@gmail.com>
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
